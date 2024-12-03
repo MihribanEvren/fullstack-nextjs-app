6ff81870a32183c2f785c49fc855cbb183d10f91
Mihriban Evren <mihribanevrenn@gmail.com>
2024-12-03 23:43:45 +0300

    completed navbar and hero section

---
+++ components/StartupCard.tsx
@@ -0,0 +1,11 @@
+const StartupCard = ({ post }: { post: StartupCardType }) => {
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p></p>
+      </div>
+    </li>
+  );
+};
+
+export default StartupCard;
